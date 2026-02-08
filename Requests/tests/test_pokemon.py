12e07e31c8e596e54655f7df733f1182fe97f93e
Alex Khramchenkov <alxhav@gmail.com>
2026-02-08 15:24:49 +0300

    Update test_pokemon.py

--- Requests/tests/test_pokemon.py
+++ Requests/tests/test_pokemon.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = '2f6761d64823d41e2eb25c6ccaaeb405'
+TOKEN = 'Token'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = '46671'
 
@@ -14,3 +14,4 @@ def test_status_code():
 def test_trainer_name():
     response_get = requests.get(url = f'{URL}/trainers/46671', headers = HEADER)
     assert response_get.json()['trainer_name'] == 'SepherotH'   
+
